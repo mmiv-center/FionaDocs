73bb72d9e79a676a9f2b76537f99723fc804f7f0
marekkoc <m2019k1020@gmail.com>
2025-07-30 12:24:22 +0200

    Architecture/scripts: setup.sh. Work in progres -> test

--- docs/source/Architecture/scripts/setup.sh
+++ docs/source/Architecture/scripts/setup.sh
@@ -7,32 +7,50 @@
 #  ln -s /var/www/html/server/bin/storectl.sh storectl.sh
 #fi
 
-
+echo
+echo "Create sybolic links to Fiona scripts (sh, py, php)..."
+echo
 
 # /home/processing/bin
-files = ("anonymizeAndSend.py"
-          "clearExport.sh"
-          "clearOldFiles.sh"
-          "clearStaleLinks.sh"
-          "createTransferRequestsForProcessed.py"
-          "createTransferRequests.py"
-          "populateAutoID.py"
-          "populateIncoming.py"
-          "populateProjects.py");
+files=("anonymizeAndSend.py"
+  "clearExport.sh"
+  "clearOldFiles.sh"
+  "clearStaleLinks.sh"
+  "createTransferRequestsForProcessed.py"
+  "createTransferRequests.py"
+  "populateAutoID.py"
+  "populateIncoming.py"
+  "populateProjects.py");
 
 for file in "${files[@]}"; do
   if [[ ! -e "$file" ]]; then
-    # create symbolic link
-    # ln - /home/processing/bin/$file $file
-    echo "$file: create link"
+    # ln -s /home/processing/bin/$file $file
+    echo "*** $file: link created"
   else
-    echo "$file: link exists"
+    echo "*** $file: link exists"
   fi
 done
 
 
+# /home/processing/bin/utils
+files=("getAllPatients2.sh"
+  "parseAllPatients.sh"
+  "resendProject.py"
+  "whatIsInIDS7.py"
+  "whatIsNotInIDS7.py");
+
+for file in "${files[@]}"; do
+  if [[ ! -e "$file" ]]; then
+    # ln -s /home/processing/bin/utils/$file $file
+    echo "*** $file: link created"
+  else
+    echo "*** $file: link exists"
+  fi
 
 
+echo
+echo "Links created..."
+echo
 
 
 
